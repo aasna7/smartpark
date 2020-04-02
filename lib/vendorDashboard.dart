9d6a0c7f4c2a3d220c5d2c346939f28b41d102ec
Saujan Bindukar <sauzanbindukar@gmail.com>
2020-04-02 11:48:22 +0545

    vendor dashboard modified

--- lib/vendorDashboard.dart
+++ lib/vendorDashboard.dart
@@ -10,72 +10,94 @@ class _VendorDashboardState extends State<VendorDashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(centerTitle: true, title: Text('Vendor Dashboard')),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: GridView.builder(
-              itemCount: 6,
-              gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 2.5,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                index = index + 1;
-                return InkWell(
-                  onTap: () {
-                    showDialog(
-                      barrierDismissible: false,
-                      context: context,
-                      child: new CupertinoAlertDialog(
-                        title: new Column(
-                          children: <Widget>[
-                            new Text("GridView"),
-                            new Icon(
-                              Icons.favorite,
-                              color: Colors.green,
+      appBar: AppBar(centerTitle: true, title: Text('Vendor Dashboard')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 4, // Number of slot of Car
+                  itemBuilder: (BuildContext context, int index) {
+                    index = index + 1;
+                    return InkWell(
+                      onTap: () {
+                        showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          child: new CupertinoAlertDialog(
+                            title: new Column(
+                              children: <Widget>[
+                                new Text("GridView"),
+                                new Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                ),
+                              ],
                             ),
-                          ],
+                            content: new Text("Car slot no. $index"),
+                            actions: <Widget>[
+                              new FlatButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: new Text("OK"))
+                            ],
+                          ),
+                        );
+                      },
+                      child: Container(
+                        height: 60,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1),
                         ),
-                        content: new Text("Selected Item $index"),
-                        actions: <Widget>[
-                          new FlatButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: new Text("OK"))
-                        ],
+                        child: Center(child: Text('Car Slot No. $index')),
                       ),
                     );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(border: Border.all(width: 1)),
-                  ),
-                );
-              }),
-        ));
-  }
-}
-
-class Slots extends StatefulWidget {
-  @override
-  _SlotsState createState() => _SlotsState();
-}
-
-class _SlotsState extends State<Slots> {
-  @override
-  Widget build(BuildContext context) {
-    final list_item = [
-      {
-        "name": "image 1",
-      },
-      {"name": "image 2"}
-    ];
-    return GridView.builder(
-        itemCount: list_item.length,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (BuildContext context, int index) {
-          // return Slot();
-        });
+                  }),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 5, // number of slot for bike
+                  itemBuilder: (BuildContext context, int index) {
+                    index = index + 1;
+                    return InkWell(
+                      onTap: () {
+                        showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          child: new CupertinoAlertDialog(
+                            title: new Column(
+                              children: <Widget>[
+                                new Text("GridView"),
+                                new Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                ),
+                              ],
+                            ),
+                            content: new Text("Bike slot no. $index"),
+                            actions: <Widget>[
+                              new FlatButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: new Text("OK"))
+                            ],
+                          ),
+                        );
+                      },
+                      child: Container(
+                        height: 40,
+                        decoration: BoxDecoration(border: Border.all(width: 1)),
+                        child: Center(child: Text('Bike Slot No. $index')),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
