7fb57ab88237c9b66baa51a4ae923b8b9a795848
Saujan Bindukar <sauzanbindukar@gmail.com>
2020-04-19 11:51:43 +0545

    button added in vendor home

--- lib/vendorHome.dart
+++ lib/vendorHome.dart
@@ -8,119 +8,125 @@ class VendorHome extends StatefulWidget {
 
 class _VendorHomeState extends State<VendorHome> {
   @override
-  void initState() {
-    super.initState();
-    // userData();
-  }
-
-  // Future<String> userData() async {
-  //   final FirebaseUser user = await FirebaseAuth.instance.currentUser();
-  //   final String email = user.email.toString();
-  //   this.setState(() {
-  //     userEmail = email;
-  //   });
-  //   print(userEmail);
-  //   return email;
-  // }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text('Vendor Home')),
-      body: Expanded(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: ListView.builder(
-                        itemCount: 4, // Number of slot of Car
-                        itemBuilder: (BuildContext context, int index) {
-                          index = index + 1;
-                          return InkWell(
-                            onTap: () {
-                              showDialog(
-                                barrierDismissible: false,
-                                context: context,
-                                child: new CupertinoAlertDialog(
-                                  title: new Column(
-                                    children: <Widget>[
-                                      new Text("GridView"),
-                                      new Icon(
-                                        Icons.favorite,
-                                        color: Colors.red,
-                                      ),
-                                    ],
-                                  ),
-                                  content: new Text("Car slot no. $index"),
-                                  actions: <Widget>[
-                                    new FlatButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: new Text("OK"))
-                                  ],
-                                ),
-                              );
-                            },
-                            child: Container(
-                              height: 150,
-                              decoration: BoxDecoration(
-                                border: Border.all(width: 1),
-                              ),
-                              child: Center(child: Text('Car Slot No. $index')),
-                            ),
-                          );
-                        }),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                        itemCount: 5, // number of slot for bike
-                        itemBuilder: (BuildContext context, int index) {
-                          index = index + 1;
-                          return InkWell(
-                            onTap: () {
-                              showDialog(
-                                barrierDismissible: false,
-                                context: context,
-                                child: new CupertinoAlertDialog(
-                                  title: new Column(
-                                    children: <Widget>[
-                                      new Text("GridView"),
-                                      new Icon(
-                                        Icons.favorite,
-                                        color: Colors.red,
-                                      ),
-                                    ],
-                                  ),
-                                  content: new Text("Bike slot no. $index"),
-                                  actions: <Widget>[
-                                    new FlatButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: new Text("OK"))
-                                  ],
-                                ),
-                              );
-                            },
-                            child: Container(
-                              height: 100,
-                              decoration:
-                                  BoxDecoration(border: Border.all(width: 1)),
-                              child:
-                                  Center(child: Text('Bike Slot No. $index')),
-                            ),
-                          );
-                        }),
-                  ),
-                ],
-              ),
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 76,
+              left: 16,
+              right: 16,
+              top: 16,
             ),
-          ],
-        ),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: 4, // Number of slot of Car
+                      itemBuilder: (BuildContext context, int index) {
+                        index = index + 1;
+                        return InkWell(
+                          onTap: () {
+                            showDialog(
+                              barrierDismissible: false,
+                              context: context,
+                              child: new CupertinoAlertDialog(
+                                title: new Column(
+                                  children: <Widget>[
+                                    new Text("GridView"),
+                                    new Icon(
+                                      Icons.favorite,
+                                      color: Colors.red,
+                                    ),
+                                  ],
+                                ),
+                                content: new Text("Car slot no. $index"),
+                                actions: <Widget>[
+                                  new FlatButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: new Text("OK"))
+                                ],
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: 100,
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                            ),
+                            child: Center(child: Text('Car Slot No. $index')),
+                          ),
+                        );
+                      }),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: 5, // number of slot for bike
+                      itemBuilder: (BuildContext context, int index) {
+                        index = index + 1;
+                        return InkWell(
+                          onTap: () {
+                            showDialog(
+                              barrierDismissible: false,
+                              context: context,
+                              child: new CupertinoAlertDialog(
+                                title: new Column(
+                                  children: <Widget>[
+                                    new Text("GridView"),
+                                    new Icon(
+                                      Icons.favorite,
+                                      color: Colors.red,
+                                    ),
+                                  ],
+                                ),
+                                content: new Text("Bike slot no. $index"),
+                                actions: <Widget>[
+                                  new FlatButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: new Text("OK"))
+                                ],
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: 80,
+                            decoration:
+                                BoxDecoration(border: Border.all(width: 1)),
+                            child: Center(child: Text('Bike Slot No. $index')),
+                          ),
+                        );
+                      }),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            left: 16,
+            right: 16,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.pinkAccent,
+              ),
+              child: Center(
+                  child: Text(
+                'Reservation',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white),
+              )),
+            ),
+          )
+        ],
       ),
     );
   }
